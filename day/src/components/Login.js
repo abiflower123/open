0309f487f700b1d76f78cc2c4fe66a8e80a7e5ef
Abi P <168949975+abiflower123@users.noreply.github.com>
2025-09-12 13:49:19 +0530

    Update Login.js

--- day/src/components/Login.js
+++ day/src/components/Login.js
@@ -17,7 +17,8 @@ const Login = () => {
     const onSubmit = async e => {
         e.preventDefault();
         try {
-            const res = await axios.post('http://localhost:5000/api/auth/login', formData);
+            const res = await axios.post('https://backend-recepie-generator.onrender.com
+/api/auth/login', formData);
             setMessage('Login successful! Redirecting...');
             console.log(res.data); // Log the token
             // Store token in localStorage
