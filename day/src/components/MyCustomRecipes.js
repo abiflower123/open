f9230e8e5b5f047c10e22ed8bf6d65168cc39add
Abi P <168949975+abiflower123@users.noreply.github.com>
2025-11-22 13:26:57 +0530

    Update MyCustomRecipes.js

--- day/src/components/MyCustomRecipes.js
+++ day/src/components/MyCustomRecipes.js
@@ -23,8 +23,7 @@ const MyCustomRecipes = () => {
             const config = {
                 headers: { 'x-auth-token': token }
             };
-            const res = await axios.get('https://backend-recepie-generator.onrender.com
-/api/custom-recipes', config);
+            const res = await axios.get('https://backend-recepie-generator.onrender.com/api/custom-recipes', config);
             setCustomRecipes(res.data);
             setLoading(false);
         } catch (err) {
@@ -49,8 +48,7 @@ const MyCustomRecipes = () => {
                 const config = {
                     headers: { 'x-auth-token': token }
                 };
-                await axios.delete(`https://backend-recepie-generator.onrender.com
-/api/custom-recipes/${recipeId}`, config);
+                await axios.delete(`https://backend-recepie-generator.onrender.com/api/custom-recipes/${recipeId}`, config);
                 alert('Recipe deleted successfully!');
                 fetchCustomRecipes(); // Re-fetch recipes to update the list
             } catch (err) {
